24c7fc5a168a2b7f1a9328bc743ef89cf9e8d8a6
James Mba Azam <jamesazam@sun.ac.za>
2021-02-27 22:51:10 +0200

    analysis of primary outcomes

--- scripts/orv_sys_rev_data_analysis.R
+++ scripts/orv_sys_rev_data_analysis.R
@@ -4,7 +4,8 @@ library('scales')
 library('janitor')
 
 #load the data and remove the Kobotoolbox meaningless variables 
-data_raw <- readxl::read_excel('./data/2021_02_18.xlsx') 
+review_data_wide <- readxl::read_excel('./data/final_data/2021_02_25_wide.xlsx') 
+review_data_compact <- readxl::read_excel('./data/final_data/2021_02_25_compact.xlsx') 
 
 
 ############################################
@@ -12,7 +13,7 @@ data_raw <- readxl::read_excel('./data/2021_02_18.xlsx')
 ############################################
 
 
-data_cleaned <- data_raw %>% 
+review_data_wide_cleaned <- review_data_wide %>% 
     select(-c('start':'phonenumber'),
            -c('__version__':'_notes')
            # -author_affiliation_type,
@@ -28,153 +29,276 @@ data_cleaned <- data_raw %>%
     clean_names() %>% 
     remove_empty('cols')
 
-
+review_data_compact_cleaned <- review_data_compact %>% 
+  select(-c('start':'phonenumber'),
+         -c('__version__':'_notes')
+         # -author_affiliation_type,
+         # -country_studied,
+         # -disease,
+         # -objectives,
+         # -model_structure,
+         # -parametrization,
+         # -validation,
+         # -intervention_modelled,
+         # -outcome_measured
+  ) %>% 
+  clean_names() %>% 
+  remove_empty('cols')
 
 #' Primary objectives 
 #' 
 #' 
-#' 1. Is modelling being used to inform policy?
+#' 1. Is modelling being used to inform policy? NB: pm = policy making
 
 # 1A. Get relevant variables
-data_policy_making <- data_cleaned %>%
+
+pm_data <- review_data_compact_cleaned %>%
   select(
     paper_title,
     publication_year,
-    starts_with("author_affiliation_type_"),
+    starts_with("objectives"),
+    starts_with("author_affiliation_type"),
+    starts_with('disease'),
     author_in_country_studied,
     outbreak_type,
-    starts_with("objectives_"),
     modelling_timing,
     data_used
   )
 
-# 1B. Clean the variable names
-data_policy_making_clean <- data_policy_making %>%
-  arrange(publication_year) %>%
-  rename_at(
-    vars(starts_with("author_affiliation_type_")),
-    funs(str_replace(
-      .,
-      "author_affiliation_type_",
-      ""
-    ))
-  ) %>%
-  rename_at(
-    vars(starts_with("objectives_")),
-    funs(str_replace(
-      .,
-      "objectives_",
-      ""
-    ))
-  ) %>%
+# 1B. Create the author affiliation types and reword the string NA
+pm_data_mutated <- pm_data %>%
   arrange(publication_year) %>%
   mutate(
-    objective = case_when(
-      assess_impact_past == T & assess_impact_future == F & assess_impact_other == F ~ "assess_impact_past",
-      assess_impact_past == F & assess_impact_future == T & assess_impact_other == F ~ "assess_impact_future",
-      assess_impact_past == F & assess_impact_future == F & assess_impact_other == T ~ "assess_impact_other"
-    ),
-    affiliation_type = case_when(
-      academic_institutions == T & government_institutions == F & ngo == F ~ "A",
-      academic_institutions == F & government_institutions == T & ngo == F ~ "G",
-      academic_institutions == F & government_institutions == F & ngo == T ~ "N",
-      academic_institutions == T & government_institutions == T & ngo == F ~ "A+G",
-      academic_institutions == T & government_institutions == F & ngo == T ~ "A+N",
-      academic_institutions == F & government_institutions == T & ngo == T ~ "G+N",
-      academic_institutions == T & government_institutions == T & ngo == T ~ "A+G+N"
-    ),
+    author_affiliation_type = str_replace_all(author_affiliation_type, ' ', ' + '),
     author_in_country_studied = if_else(author_in_country_studied == 'NA', 'not_applicable',
                                         author_in_country_studied
                                         )
-  )
+    )
 
-#' Count by affiliation type and plot
-data_policy_making_affiliation_type_aggregated <- data_policy_making_clean %>% 
-    mutate(affiliation_type = as_factor(affiliation_type)) %>% 
-    count(affiliation_type) %>% 
-    mutate(perc = round(n/sum(n)*100, 1)) 
+
+
+#' Count by affiliation type and make a barplot
+pm_data_affiliation_aggregated <- pm_data_mutated %>% 
+    mutate(author_affiliation_type = as_factor(author_affiliation_type)) %>% 
+    count(author_affiliation_type) %>% 
+    mutate(perc = round(n/sum(n)*100, 1)) %>% 
+    arrange(desc(n))
  
     
     
-author_affilition_type_aggregated_plot <- data_policy_making_affiliation_type_aggregated %>%
-    ggplot() + geom_bar(aes(x = reorder(affiliation_type, perc),
+affiliation_type_aggregated_barplot <- pm_data_affiliation_aggregated %>%
+    ggplot() + geom_bar(aes(x = reorder(author_affiliation_type, perc),
                             y = perc,
-                            fill = factor(affiliation_type)
+                            fill = factor(author_affiliation_type)
                             ),
                         stat = 'identity',
                         color = 'black'
                         ) +
     scale_y_continuous(breaks = seq(0, 60, 5),
                        labels = seq(0, 60, 5)) +
-    labs(x = 'Author affiliation type', y = 'Percent') + 
+    labs(title = 'Aggregated number of studies per author affiliation type', 
+         x = 'Author affiliation type', 
+         y = 'Percent') + 
+ #   coord_polar(theta = "x", direction = -1) +
     scale_color_tableau()
 
-plot(author_affilition_type_aggregated_plot)
+plot(affiliation_type_aggregated_barplot)
+
 
 #' Save the plot    
-ggsave(filename = './figs/author_affilition_type_aggregated_plot.jpg', 
-       plot = author_affilition_type_aggregated_plot,
+ggsave(filename = './figs/affilition_type_aggregated_barplot.jpg', 
+       plot = affilition_type_aggregated_barplot,
        width = 10.5,
        height = 6.5,
        units = 'in'
 )
 
-#Author affiliations composition over time
-data_policy_making_trend <- data_policy_making %>%
-    arrange(publication_year) %>%
-    rename_at(
-        vars(starts_with("author_affiliation_type_")),
-        funs(str_replace(
-            .,
-            "author_affiliation_type_",
-            ""
-        ))
-    ) %>%
-    rename_at(
-        vars(starts_with("objectives_")),
-        funs(str_replace(
-            .,
-            "objectives_",
-            ""
-        ))
-    ) %>%
-    arrange(publication_year) 
+#' alternatively, make a treemap
+
+library(treemapify)
+affilition_type_aggregated_treemap <- ggplot(pm_data_affiliation_aggregated,
+                                                    aes(area = n, 
+                                                        label = perc,
+                                                        fill = author_affiliation_type
+                                                        ),
+                                                    color = 'black',
+                                                    stroke = 5,
+                                                    stat = 'identity',
+                                                    color = 'black'
+                                                    ) + 
+  geom_treemap() +
+  geom_treemap_text(place = 'centre') +
+  labs(title = 'Aggregated number of studies per author affiliation type') +
+  scale_color_tableau()
+
+        
+
+plot(affilition_type_aggregated_treemap)
 
 
-#' Count the number of each affiliation type per year
-data_policy_making_affiliation_type_by_year <- data_policy_making_clean %>% 
+#' Save the plot    
+ggsave(filename = './figs/affilition_type_aggregated_treemap.jpg', 
+       plot = affilition_type_aggregated_treemap,
+       width = 10.5,
+       height = 6.5,
+       units = 'in'
+)
+
+
+#' Count the number of each affiliation type per year (including FMD)
+pm_data_affiliation_type_FMD_only_by_year <- pm_data_mutated %>% 
+  arrange(publication_year) %>% 
+  filter(disease == 'FMD') %>% 
+  group_by(publication_year, author_affiliation_type) %>% 
+  count(publication_year, author_affiliation_type)
+
+
+pm_data_affiliation_type_wFMD_by_year <- pm_data_mutated %>% 
     arrange(publication_year) %>% 
-    group_by(publication_year, affiliation_type) %>% 
-    count(publication_year, affiliation_type)
+    group_by(publication_year, author_affiliation_type) %>% 
+    count(publication_year, author_affiliation_type)
 
-#Plot the affiliation type over time
-author_affiliation_type_by_year_plot <- ggplot(data = data_policy_making_affiliation_type_by_year) + 
-    geom_bar(aes(x = factor(publication_year), 
+
+#' Count the number of each affiliation type per year (excluding FMD)
+pm_data_affiliation_type_no_FMD_by_year <- pm_data_mutated %>% 
+  arrange(publication_year) %>% 
+  filter(disease != 'FMD') %>% 
+  group_by(publication_year, author_affiliation_type) %>% 
+  count(publication_year, author_affiliation_type)
+
+#Plot the affiliation type over time (with FMD)
+affiliation_type_FMD_only_by_year_barplot <- ggplot(data = pm_data_affiliation_type_FMD_only_by_year) + 
+    geom_bar(aes(x = publication_year, 
                  y = n, 
-                 fill = affiliation_type
+                 fill = author_affiliation_type
                  ),
              color = 'black',
              stat = 'identity',
              position = 'stack'
              ) + 
-    labs(title = 'Author affiliation composition over time',
+    scale_x_continuous(limits = c(min(pm_data_affiliation_type_wFMD_by_year$publication_year), 
+                                  max(pm_data_affiliation_type_wFMD_by_year$publication_year)
+                                  ),
+                       breaks = seq(min(pm_data_affiliation_type_wFMD_by_year$publication_year), 
+                                    max(pm_data_affiliation_type_wFMD_by_year$publication_year),
+                                    5
+                                    ),
+                       labels = seq(min(pm_data_affiliation_type_wFMD_by_year$publication_year), 
+                                  max(pm_data_affiliation_type_wFMD_by_year$publication_year),
+                                  5
+                                  )
+                       ) +
+  scale_y_continuous(limits = c(0, 
+                                max(pm_data_affiliation_type_wFMD_by_year$n)
+                                ), 
+                     breaks = seq(0, 
+                                  max(pm_data_affiliation_type_wFMD_by_year$n), 
+                                  1
+                                  ), 
+                     labels = seq(0, 
+                                  max(pm_data_affiliation_type_wFMD_by_year$n), 
+                                  1
+                                  )
+                     ) +
+    labs(title = 'Author affiliation composition over time (FMD only)',
          x = 'Year',
          y = 'Number of publications') +
     scale_color_tableau()
 
-plot(author_affiliation_type_by_year_plot)
+plot(affiliation_type_FMD_only_by_year_barplot)
 
 #' save the plot
-ggsave(filename = './figs/author_affiliation_type_by_year_plot.jpg', 
-       plot = author_affiliation_type_by_year_plot,
+ggsave(filename = './figs/affiliation_type_FMD_only_by_year_barplot.jpg', 
+       plot = affiliation_type_FMD_only_by_year_barplot,
        width = 10.5,
        height = 6.5,
        units = 'in'
        )
 
-#' 2. Is vaccination the most effective when compared to other single interventions?
 
-data_disease_intervention_vax_vars <- data_cleaned %>% 
+#' Count the number of each author affiliation type per year (including FMD)
+pm_data_affiliation_type_wFMD_by_year <- pm_data_mutated %>% 
+  arrange(publication_year) %>% 
+  group_by(publication_year, author_affiliation_type) %>% 
+  count(publication_year, author_affiliation_type)
+
+
+#Plot the affiliation type over time (with FMD)
+affiliation_type_by_year_wFMD_barplot <- ggplot(data = pm_data_affiliation_type_wFMD_by_year) + 
+  geom_bar(aes(x = publication_year, 
+               y = n, 
+               fill = author_affiliation_type
+  ),
+  color = 'black',
+  stat = 'identity',
+  position = 'stack'
+  ) + 
+  labs(title = 'Author affiliation composition per year (with FMD)',
+       x = 'Year',
+       y = 'Number of publications') +
+  scale_color_tableau()
+
+plot(affiliation_type_by_year_wFMD_barplot)
+
+#' save the plot
+ggsave(filename = './figs/affiliation_type_by_year_with_FMD_barplot.jpg', 
+       plot = affiliation_type_by_year_wFMD_barplot,
+       width = 10.5,
+       height = 6.5,
+       units = 'in'
+)
+
+
+#' Count the number of each author affiliation type per year (excluding FMD)
+pm_data_affiliation_type_no_FMD_by_year <- pm_data_mutated %>% 
+  arrange(publication_year) %>% 
+  filter(disease != 'FMD') %>% 
+  group_by(publication_year, author_affiliation_type) %>% 
+  count(publication_year, author_affiliation_type)
+
+
+#Plot the affiliation type over time (with FMD)
+affiliation_type_by_year_no_FMD_barplot <- ggplot(data = pm_data_affiliation_type_no_FMD_by_year) + 
+  geom_bar(aes(x = publication_year, 
+               y = n, 
+               fill = author_affiliation_type
+  ),
+  color = 'black',
+  stat = 'identity',
+  position = 'stack'
+  ) + 
+  labs(title = 'Author affiliation composition per year (without FMD)',
+       x = 'Year',
+       y = 'Number of publications') +
+  scale_color_tableau()
+
+plot(affiliation_type_by_year_no_FMD_barplot)
+
+#' save the plot
+ggsave(filename = './figs/affiliation_type_by_year_no_FMD_barplot.jpg', 
+       plot = affiliation_type_by_year_no_FMD_barplot,
+       width = 10.5,
+       height = 6.5,
+       units = 'in'
+       )
+
+ #Plot the data with and without FMD
+library(patchwork)
+
+affiliation_type_by_year_with_and_without_FMD_barplot <- plot(affiliation_type_by_year_no_FMD_barplot/affiliation_type_by_year_wFMD_barplot)
+
+#' save the plot
+ggsave(filename = './figs/affiliation_type_by_year_with_and_without_FMD_barplot.jpg', 
+       plot = affiliation_type_by_year_with_and_without_FMD_barplot,
+       width = 10.5,
+       height = 6.5,
+       units = 'in'
+)
+
+
+#' 2. Is vaccination the most effective when compared to other single interventions?
+#' the wide data
+vax_vars_wide_data <- review_data_wide_cleaned %>% 
     select(paper_title, 
            publication_year,
            starts_with('disease_'),
@@ -184,8 +308,74 @@ data_disease_intervention_vax_vars <- data_cleaned %>%
            is_vax_effective
            ) 
 
+#the long data
+vax_vars_data_no_FMD_compact <- review_data_compact_cleaned %>% 
+  select(paper_title, 
+         publication_year,
+         starts_with('disease'),
+         outbreak_type,
+         starts_with('intervention_modelled'),
+         vax_modelled_with_non_vax,
+         is_vax_effective
+  ) %>% 
+  filter(vax_modelled_with_non_vax == 'yes', disease != 'FMD')
+
+#count the impact categories
+vax_impact_categories <- vax_vars_data_no_FMD_compact %>% 
+  count(is_vax_effective) %>% 
+  mutate(perc = n/sum(n)*100) %>% 
+  arrange(n)
+
+
+vax_impact_categories
+
+
+#Plot the aggregated vaccine impact results
+
+vax_impact_dot_plot <- ggplot(data = vax_impact_categories) + 
+  geom_bar(
+    aes(x = is_vax_effective, 
+        y = n,
+        fill = is_vax_effective
+        ),
+    stat = 'identity',
+    width = 0.25
+    ) +
+  scale_y_continuous(breaks = seq(0, 
+                                  max(vax_impact_categories$n),
+                                  2
+                                  ),
+                     labels = seq(0, 
+                                max(vax_impact_categories$n),
+                                2
+                                )
+                     ) +
+  scale_x_discrete(breaks = c('no', 
+                              'yes', 
+                              'the_outcomes_were_inconclusive'
+                              ), 
+                   labels = c('no' = 'no', 
+                              'yes' = 'yes', 
+                              'the_outcomes_were_inconclusive' = 'inconclusive'
+                              )
+                   ) +
+  scale_fill_discrete(name = 'Vaccine impact',
+                      labels = c('no', 
+                                 'inconclusive',
+                                 'yes'
+                      )
+                      ) +
+  labs(x = 'Vaccine is most impactful single intervention',
+       y = 'Number of studies'
+       ) +
+  scale_color_colorblind()
+
+plot(vax_impact_dot_plot)
+
+
+
 #' Clean the data by renaming some columns
-data_disease_intervention_vax_vars_clean <- data_disease_intervention_vax_vars %>%
+vax_vars_wide_data_cleaned <- vax_vars_wide_data %>%
     arrange(publication_year) %>%
     rename_at(
         vars(starts_with("disease_")),
@@ -205,23 +395,73 @@ data_disease_intervention_vax_vars_clean <- data_disease_intervention_vax_vars %
     )
 
 
+#vaccination impact data
+data_vax_impact <- data_disease_intervention_vax_vars_clean %>% 
+  filter(vax_modelled_with_non_vax == 'yes') %>% 
+  count(is_vax_effective) %>% 
+  mutate(perc = n/sum(n)*100) 
+
+#vaccination impact plot
+data_vax_impact_barplot <- data_vax_impact %>% 
+  ggplot(aes(x = is_vax_effective,
+             y = perc,
+             fill = is_vax_effective
+             )
+         ) + 
+  geom_bar(stat = 'identity') +
+  scale_color_tableau()
+  
+plot(data_vax_impact_barplot)
+
+#' save the plot
+ggsave(filename = './figs/data_vax_impact_barplot.jpg', 
+       plot = data_vax_impact_barplot,
+       width = 10.5,
+       height = 6.5,
+       units = 'in'
+)
 
 #' Secondary objectives
 
 #' 1. Number of ORV publications by year
-data_cleaned %>% count(publication_year) %>% 
+#' FMD included
+pubs_per_year_with_FMD_bar_plot <- review_data_compact_cleaned %>% 
+  count(publication_year) %>% 
     ggplot() + 
-    geom_bar(aes(x = publication_year, y = n), 
-             stat = 'identity',
-             color = 'black',
-             fill = 'tomato3'
-    ) +
+    geom_bar(aes(x = publication_year, 
+                 y = n
+    ), 
+    stat = 'identity',
+    color = 'black',
+    fill = 'tomato3') +
     scale_x_continuous(breaks = seq(1970, 2020, 5),
                        labels = seq(1970, 2020, 5)
     ) +
     scale_y_continuous(breaks = seq(0, 26, 2),
                        labels = seq(0, 26, 2)
     )
+
+plot(pubs_per_year_with_FMD_bar_plot)
+
+#' FMD excluded
+pubs_per_year_no_FMD_bar_plot <- review_data_compact_cleaned %>% 
+  filter(disease != 'FMD') %>% 
+  count(publication_year) %>% 
+  ggplot() + 
+  geom_bar(aes(x = publication_year, 
+               y = n
+  ), 
+  stat = 'identity',
+  color = 'black',
+  fill = 'tomato3') +
+  scale_x_continuous(breaks = seq(1970, 2020, 5),
+                     labels = seq(1970, 2020, 5)
+  ) +
+  scale_y_continuous(breaks = seq(0, 26, 2),
+                     labels = seq(0, 26, 2)
+  )
+
+plot(pubs_per_year_no_FMD_bar_plot)
 
 
 #' Year with highest number of ORV publications
@@ -233,7 +473,7 @@ max_pub_by_year
 
 
 #' Disease and intervention(s) modelled
-data_disease_and_intervention <- data_cleaned %>% 
+data_disease_and_intervention <- review_data_wide_cleaned %>% 
     select(publication_year,
            starts_with('disease_'),
            starts_with('intervention_modelled_'),
